9ec6124ac2c862369000ee098fade6a3374c02d0
dell <dell@DESKTOP-BC56FV6>
2019-10-04 20:27:32 -0700

    test commit adds rollDice method that returns an int between 1 and 6

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,16 @@
+import java.util.Random;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		System.out.println(rollDice());
 	}
-
+// returns a random int between 1 and 6
+	private static int rollDice()
+	{
+		Random rand = new Random();
+		return rand.nextInt(6) + 1;
+	}
 }
